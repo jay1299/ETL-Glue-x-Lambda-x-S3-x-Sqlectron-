795f4c5ddeeec6d889ca6ad2784efb85d8bb53d2
Jay Shah <jayshah1299@gmail.com>
2023-05-29 00:53:27 +0530

    Lambda function to trigger ETL job

---
+++ Lambda2.py
@@ -0,0 +1,17 @@
+# Set up logging
+import json
+import os
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+import boto3
+client = boto3.client('glue')
+
+glueJobName = "Job1"
+
+# Define Lambda function
+def lambda_handler(event, context):
+    response=client.start_crawler(Name='TargetS3')
+    response = client.start_job_run(JobName = glueJobName)
+    return response
